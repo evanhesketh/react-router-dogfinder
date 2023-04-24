afcaf3821e158ec03e94014c62279ed935940327
Evan Hesketh <evanhesketh@yahoo.ca>
2023-04-24 17:56:28 -0400

    add DogDetails

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import DogDetails from "./DogDetails";
+
+const dogs = [
+	{
+		"name": "Whiskey",
+		"age": 5,
+		"src": "whiskey",
+		"facts": [
+			"Whiskey loves eating popcorn.",
+			"Whiskey is a terrible guard dog.",
+			"Whiskey wants to cuddle with you!"
+		]
+	},
+	{
+		"name": "Duke",
+		"age": 3,
+		"src": "duke",
+		"facts": [
+			"Duke believes that ball is life.",
+			"Duke likes snow.",
+			"Duke enjoys pawing other dogs."
+		]
+	},
+	{
+		"name": "Perry",
+		"age": 4,
+		"src": "perry",
+		"facts": [
+			"Perry loves all humans.",
+			"Perry demolishes all snacks.",
+			"Perry hates the rain."
+		]
+	}
+]
 
 function App() {
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <h1>Dog Finder!</h1>
+      <BrowserRouter>
+        {/* <Nav /> */}
+        <Routes>
+          {/* <Route path="/dogs" element={<DogList />} /> */}
+          <Route path="/dogs/:name" element={<DogDetails dogs={dogs}/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/DogDetails.js
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+
+function DogDetails({dogs}) {
+  const {name} = useParams();
+
+  const dog = dogs.filter(dog => dog.name === name)[0];
+
+  return (
+    <div className="DogDetails">
+      <h2 className="DogDetails-name">{dog.name}</h2>
+      <img src={`../${dog.src}.jpg`} alt={dog.name}></img>
+      <p className="DogDetails-age">Age: {dog.age}</p>
+      Facts:
+      <ul className="DogDetails-facts">
+        {dog.facts.map(fact => <li>{fact}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default DogDetails;
